36ae1cdbc9895904b8a1a3423ca492dad8ca2b71
DimaObora <obora.dima@yandex.ru>
2018-02-26 13:51:02 +0300

    переделал слайдер

--- js/main.js
+++ js/main.js
@@ -210,3 +210,22 @@ function testBlock() {
 */
 /*end RIGHT sitbar*/
 
+$(document).ready(function() {
+
+    // Expand Panel
+    $("#open").click(function(){
+        $("div#sliding_panel").slideDown("slow");
+
+    });
+
+    // Collapse Panel
+    $("#close").click(function(){
+        $("div#sliding_panel").slideUp("slow");
+    });
+
+    // Switch buttons from "Log In | Register" to "Close Panel" on click
+    $("#toggle a").click(function () {
+        $("#toggle a").toggle();
+    });
+
+});
